874ff0e1ba6b1c058c4674e2eea403a503e4ee93
Priyankagurumella06 <priyankagurumella06@gmail.com>
2026-08-04 22:15:18 +0530

    Add files via upload

---
+++ FruitDemo.java
@@ -0,0 +1,39 @@
+class Fruit {
+    String name;
+    String taste;
+    String size;
+    Fruit(String name, String taste, String size) {
+        this.name = name;
+        this.taste = taste;
+        this.size = size;
+    }
+    void eat() {
+        System.out.println(name + " tastes " + taste);
+    }
+}
+class Apple extends Fruit {
+    Apple() {
+        super("Apple", "Sweet", "Medium");
+    }
+    @Override
+    void eat() {
+        System.out.println(name + " tastes " + taste);
+    }
+}
+class Orange extends Fruit {
+    Orange() {
+        super("Orange", "Sour", "Medium");
+    }
+    @Override
+    void eat() {
+        System.out.println(name + " tastes " + taste);
+    }
+}
+public class FruitDemo {
+    public static void main(String[] args) {
+        Apple a = new Apple();
+        Orange o = new Orange();
+        a.eat();
+        o.eat();
+    }
+}
